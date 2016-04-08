c956a23192af44fae64e3ff0c8d9b60f2328eb87
Christoph Giesche <christoph.giesche@gmx.net>
2016-04-08 19:18:11 +0200

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,89 @@
+**Contexts and Dependency Injection for JavaFX**
+
+This library allows you to use Contexts and Dependency Injection in your JavaFX application. It uses Weld as the underlying CDI implementation.
+
+To get started, first make sure, you have a `beans.xml` file created inside your META-INF directory:
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://xmlns.jcp.org/xml/ns/javaee" bean-discovery-mode="all">
+</beans>
+```
+
+Then simply create a class that extends `FxWeldApplication`. It is very similar to the standard `Application` class from JavaFX core and can be used equally. It also provides a main-method to start the CDI container and your JavaFX application.
+
+```
+public class Main extends FxWeldApplication {
+
+    @Inject
+    @Bundle("bundles/Application")
+    private final FXMLLoader fxmlLoader;
+
+    public void start(Stage stage, Application.Parameters parameters) throws Exception {
+        final Parent mainWindow = fxmlLoader.load(getClass().getResourceAsStream("application.fxml"));
+        final Scene scene = new Scene(mainWindow);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
+```
+
+As you can see, the `FXMLLoader` is already being injected by CDI. The `@Bundle` annotation assigns the provided `ResourceBundle` to the injected `FXMLLoader`. An instance of the controller class defined in the FXML-File will automatically be created and managed within the CDI context. 
+
+```
+<VBox fx:controller="de.perdoctus.fx.Contoller">
+   <children>
+      <Label text="%label-text" />
+      <Button text="%button-text" />
+   </children>
+</VBox>
+
+```
+
+From now on, you are fully CDI enabled. You can write your own producers, inject everything into controllers and even use CDI events.
+
+```
+public class Controller {
+    
+    @Inject
+    private XyService xyService;
+    
+    @Inject
+    @Bundle
+    private ResourceBundle rb;
+    
+    @Inject
+    private Event<FooBar> foobarEvent;
+    
+    public void on(@Observes Event<OtherEvent> event) {
+        ...
+    }
+    
+    ...
+}
+```
+ 
+**Cheat-Sheet**
+
+This library includes producers for `FXMLLoader` and `ResourceBundle` classes. This is, how you can use them: 
+
+Inject FXMLLoader:
+```
+@Inject
+private FXMLLoader fxmlLoader;
+```
+
+Inject FMXLLoader with "included" ResourceBundle:
+```
+Inject
+@Bundle("resourceBundles/General")
+private FXMLLoader fxmlLoader;
+```
+
+Inject a ResourceBundle:
+```
+@Inject
+@Bundle("LoginDialogBundle")
+private ResourceBundle loginDialogRb;
+```
+
---
+++ src/main/java/de/perdoctus/fx/FmxlLoaderProducer.java
@@ -0,0 +1,72 @@
+package de.perdoctus.fx;
+
+/*
+ * #%L
+ * javafx-weld-bootstrap
+ * %%
+ * Copyright (C) 2016 Christoph Giesche
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
+
+/**
+ * @author Christoph Giesche
+ */
+public final class FmxlLoaderProducer {
+
+    final WeldContainer weldContainer;
+
+    @Inject
+    public FmxlLoaderProducer(final WeldContainer weldContainer) {
+        this.weldContainer = weldContainer;
+    }
+
+    @Produces
+    public FXMLLoader produce(final InjectionPoint injectionPoint) {
+        final Bundle annotation = injectionPoint.getAnnotated().getAnnotation(Bundle.class);
+
+        final ResourceBundle resourceBundle;
+        if (annotation != null) {
+            resourceBundle = ResourceBundle.getBundle(annotation.value());
+        } else {
+            resourceBundle = null;
+        }
+
+        return new FXMLLoader(null, resourceBundle, new JavaFXBuilderFactory(), controllerClass -> {
+            final Object controller = weldContainer.instance().select(controllerClass).get();
+            if (controller == null) {
+                throw new RuntimeException("Failed to look up Controller of type " + controllerClass.getName() + " from ServiceLocator.");
+            }
+            return controller;
+        }, StandardCharsets.UTF_8);
+    }
+
+}
